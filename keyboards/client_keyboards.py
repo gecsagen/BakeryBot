db58b9917f12d0897d511991abfad676eb5a8c23
Karmickle <gjfjfhhhf@gmail.com>
2021-11-13 21:31:06 -0800

    Created keyboard for users

--- keyboards/client_keyboards.py
+++ keyboards/client_keyboards.py
@@ -1,1 +1,14 @@
 """Пользовательские клавиатуры"""
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+#  создаем кнопки
+button_products = KeyboardButton('Продукция')
+button_gallery = KeyboardButton('Галерея')
+button_address = KeyboardButton('Адрес')
+button_contacts = KeyboardButton('Контакты')
+
+#  создаем клавиатуру из кнопок вместо обычной
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+#  добавляем кнопки на клавиатуру в строку
+kb_client.row(button_products, button_gallery).row(button_address, button_contacts)
