d0ed1bc93cbcdd2e3684d21dc65918098412d278
Karmickle <gjfjfhhhf@gmail.com>
2021-11-13 23:12:37 -0800

    Created handler for display address

--- handlers/client_handlers.py
+++ handlers/client_handlers.py
@@ -1,6 +1,7 @@
 """Клиентские диспетчеры"""
 from aiogram import types, Dispatcher
 from keyboards import client_keyboards
+from aiogram.dispatcher.filters import Text
 
 
 async def commands_start(message: types.Message):
@@ -9,8 +10,15 @@ async def commands_start(message: types.Message):
     await message.answer('Привет! Это бот завьяловской пекарни:)', reply_markup=client_keyboards.kb_client)
 
 
+async def load_address(message: types.Message):
+    """Хендлер для отображения адреса пекарни"""
+    await message.reply('Омская обл, Знаменский Р-н, с.Завьялово, ул.Комарова 25')
+    await message.answer_location(57.148317, 73.436807)
+
+
 def register_handlers_client(dp: Dispatcher):
     """
         Функция регистратор клиентских диспетчеров, вызывается из main.py
     """
     dp.register_message_handler(commands_start, commands=['start', 'help'])
+    dp.register_message_handler(load_address, Text(startswith='Адрес'))
