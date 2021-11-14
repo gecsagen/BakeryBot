1bbaa0459adab109d143b4f1391eea05ff33665e
Karmickle <gjfjfhhhf@gmail.com>
2021-11-14 04:57:22 -0800

    Created class FSMProduct for states

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -2,6 +2,15 @@
 from aiogram import types, Dispatcher
 from loader import admins
 from keyboards.admin_keyboards import kb_admin
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class FSMProduct(StatesGroup):
+    """Класс машины состояний для добавления товаров"""
+    photo = State()  # состояние для фотографии
+    name = State()  # состояние для имени
+    description = State()  # состояние для описания
+    price = State()  # состояние для цены
 
 
 async def cm_start(message: types.Message):
