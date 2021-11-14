8254c7ae6673ceab37496f5725ba570561f607d9
Karmickle <gjfjfhhhf@gmail.com>
2021-11-14 00:27:31 -0800

    Created keyboard for admin

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,12 +1,13 @@
 """Админские диспетчеры"""
 from aiogram import types, Dispatcher
 from loader import admins
+from keyboards.admin_keyboards import kb_admin
 
 
 async def cm_start(message: types.Message):
     """Отправляет админское меню администратору"""
     if str(message.from_user.id) in admins:
-        await message.reply('Привет, админ :D')
+        await message.reply('Привет, админ :D', reply_markup=kb_admin)
 
 
 def register_handlers_admin(dp: Dispatcher):
--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -1,1 +1,15 @@
 """Админские клавиатуры"""
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+#  создаем кнопки
+button_add_product = KeyboardButton('Добавить продукт🍞')
+button_add_gallery = KeyboardButton('Добавить в галерею🌅')
+button_del_product = KeyboardButton('Удалить продукт❌')
+button_del_gallery = KeyboardButton('Удалить из галереи❌')
+button_load_timetable = KeyboardButton('Загрузить расписание🗒')
+#  создаем клавиатуру из кнопок вместо обычной
+kb_admin = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+#  добавляем кнопки на клавиатуру в строку
+kb_admin.row(button_add_product, button_del_product).row(button_add_gallery, button_del_gallery) \
+    .add(button_load_timetable)
