96eed39c146ca3327f1181612142745e967d6bcf
Karmickle <gjfjfhhhf@gmail.com>
2021-11-16 06:42:22 -0800

    add file for run bot

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+
+call venv\Scripts\activate
+
+cd BakeryBot
+
+set TOKEN=ВАШ ТОКЕН БОТА
+set ADMINS=1755807234$1730361155
+set ADDRESS=адрес предприятия$57.145600$73.441245
+set PHONES=79993847484$799974658465
+set NAMES=Василий$Иван
+set EMAIL=ouremail@gmail.com
+python app.py
+
+pause
